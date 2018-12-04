7e7a4dd35ff44658e82575f02fd1a9d3f0de92f1
cloped <educarvalholopes98@gmail.com>
2018-12-04 03:05:01 -0400

    add css, bootstrap load and env configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-let plugins = [];
+const devMode = (argv) => {
+  return argv.mode === 'development';
+}
 
-plugins.push(new HtmlWebpackPlugin({
-  template: './main/index.html',
-  chunks: ['app'],
-  filename: 'index.html',
-}));
-
-module.exports = {
+module.exports = (env, argv) => ({
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
 
   output: {
@@ -19,5 +18,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 
-  plugins
-}
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: 'index.html',
+      template: './main/index.html',
+      chunks: ['app'],
+      minify: devMode(argv) ? {} : {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      }
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: devMode(argv) ? '[name].css' : '[name].optimize.css',
+      chunkFilename: devMode(argv) ? '[id].css' : '[id].optimize.css',
+    }),
+
+    new CleanWebpackPlugin(['dist']),
+
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    })
+  ],
+});
