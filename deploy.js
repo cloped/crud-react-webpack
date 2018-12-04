f40ec378ea712529d4ea4f456204c882f36934cb
cloped <educarvalholopes98@gmail.com>
2018-12-04 10:35:05 -0400

    fix gh pages deploy

--- deploy.js
+++ deploy.js
@@ -1,11 +1,19 @@
 console.log('Making deploy!');
 
 var ghpages = require('gh-pages');
+var flag = true;
 
 ghpages.publish('dist', {
   branch: 'gh-pages',
-  repo: 'https://github.com/cloped/crud-react-webpack.git',
+  repo: 'git@github.com:cloped/crud-react-webpack.git',
   add: true,
   message: 'auto-generated commit deploy',
   push: true,
-}, function(err) {});
+}, function(err) {
+  console.log('Deploy didnt worked');
+  flag = false;
+});
+
+if (flag) {
+  console.log('Deployed!');
+}
