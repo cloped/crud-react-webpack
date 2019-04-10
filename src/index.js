22d55ea75b76278ef5fdd4141511ac976a08f45d
Eduardo Lopes <eduardo.lopes@neemu.com>
2019-04-10 12:41:42 -0400

    feat: add babel and lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
- 
+
 console.log('hello world');
 
 $('.jumbotron').append(' Jquery here');
+
+const essix = async () => {
+  await console.log('es6');
+};
+
+essix();
